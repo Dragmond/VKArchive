8e0d64c524d4eee307a3d61c2ef1106f18b6ac7c
Dragmond <124439189+Dragmond@users.noreply.github.com>
2026-07-03 01:35:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ from PySide6.QtWidgets import QApplication
 
 from gui.main_window import MainWindow
 
+from utils.logger import logger
+
+from utils.constants import APP_NAME
+
 
 def main():
 
+    logger.info("Application started")
+
     app = QApplication(sys.argv)
 
-    app.setApplicationName("VK Archive")
+    app.setApplicationName(APP_NAME)
 
     window = MainWindow()
 
