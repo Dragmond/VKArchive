5ffb08eaeb8c4f66d91e4a6f4b5ca580d2457270
Dragmond <124439189+Dragmond@users.noreply.github.com>
2026-07-07 16:14:55 +0300

    Update date_formatter.py

--- media/date_formatter.py
+++ media/date_formatter.py
@@ -6,13 +6,19 @@ from datetime import datetime
 class DateFormatter:
 
     @staticmethod
-    def key(timestamp: int) -> str:
+    def key(
+        timestamp: int,
+    ) -> str:
+
         return datetime.fromtimestamp(
             timestamp,
         ).strftime("%Y-%m-%d")
 
     @staticmethod
-    def title(timestamp: int) -> str:
+    def title(
+        timestamp: int,
+    ) -> str:
+
         return datetime.fromtimestamp(
             timestamp,
         ).strftime("%d.%m.%Y")
